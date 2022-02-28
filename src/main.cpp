107e2cb2b81e15345f99c55ce67679aa62a652bd
Masonrf <masonrfleck@gmail.com>
2022-02-28 15:51:13 -0600

    add audio

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,27 @@
 #define TRIG_PIN 13
 #define ECHO_PIN 12 
 
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioSynthWaveformSine   sine4;          //xy=323.00000381469727,563.0000324249268
+AudioSynthWaveformSine   sine2;          //xy=327.00000381469727,460
+AudioSynthWaveformSine   sine3;          //xy=329.00000381469727,511.00003242492676
+AudioSynthWaveformSine   sine1;          //xy=330.00000381469727,405
+AudioMixer4              mixer1;         //xy=616.0000076293945,471.00000762939453
+AudioOutputMQS           mqs1;           //xy=770.0000114440918,477.4000072479248
+AudioConnection          patchCord1(sine4, 0, mixer1, 3);
+AudioConnection          patchCord2(sine2, 0, mixer1, 1);
+AudioConnection          patchCord3(sine3, 0, mixer1, 2);
+AudioConnection          patchCord4(sine1, 0, mixer1, 0);
+AudioConnection          patchCord5(mixer1, 0, mqs1, 0);
+// GUItool: end automatically generated code
+
+
 //Define state machine variables that will control the state of the machine
 int state = 0;
 
