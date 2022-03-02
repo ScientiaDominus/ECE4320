9fbad24385cceddd2f6a33c803005cfa8c3a5134
Masonrf <masonrfleck@gmail.com>
2022-03-02 15:58:49 -0600

    init audio objects

--- src/main.cpp
+++ src/main.cpp
@@ -6,29 +6,24 @@
 #include "Stepper.h"
 #include "project2.h"
 #include "helper.h"
+#include "proj_2_audio.h"
  
 
-#include <Audio.h>
-#include <Wire.h>
-#include <SPI.h>
-#include <SD.h>
-#include <SerialFlash.h>
-
-// GUItool: begin automatically generated code
-AudioSynthWaveformSine   sine4;          //xy=323.00000381469727,563.0000324249268
-AudioSynthWaveformSine   sine2;          //xy=327.00000381469727,460
-AudioSynthWaveformSine   sine3;          //xy=329.00000381469727,511.00003242492676
-AudioSynthWaveformSine   sine1;          //xy=330.00000381469727,405
-AudioMixer4              mixer1;         //xy=616.0000076293945,471.00000762939453
-AudioOutputMQS           mqs1;           //xy=770.0000114440918,477.4000072479248
-AudioConnection          patchCord1(sine4, 0, mixer1, 3);
-AudioConnection          patchCord2(sine2, 0, mixer1, 1);
-AudioConnection          patchCord3(sine3, 0, mixer1, 2);
-AudioConnection          patchCord4(sine1, 0, mixer1, 0);
-AudioConnection          patchCord5(mixer1, 0, mqs1, 0);
-// GUItool: end automatically generated code
-
 void setup() {
+    AudioMemory(20);
+
+    sine1.frequency(noteFreq[NOTE_C6]);
+    sine1.amplitude(1);
+
+    sine2.frequency(noteFreq[NOTE_D6]);
+    sine2.amplitude(1);
+
+    sine3.frequency(noteFreq[NOTE_E6]);
+    sine3.amplitude(1);
+
+    sine4.frequency(noteFreq[NOTE_B5]);
+    sine4.amplitude(1);
+
     stepper_1.setSpeed(rolePerMinute);
     stepper_2.setSpeed(rolePerMinute);
     stepper_3.setSpeed(rolePerMinute);
