d7a519e42891de97c3a77650008f5b4724762fc3
Cameron Young <cameronky99@gmail.com>
2022-03-02 15:07:51 -0600

    Added pin header file

---
+++ include/pins.h
@@ -0,0 +1,53 @@
+//Ultrasonic Sensor Pins
+#define US1_TRIG_PIN 16 //Input for Ultrasonic Sensor
+#define US1_ECHO_PIN 17 //Echo for Ultrasonic Sensor
+
+#define US2_TRIG_PIN 18
+#define US2_ECHO_PIN 19
+
+#define US3_TRIG_PIN 20
+#define US3_ECHO_PIN 21
+
+#define US4_TRIG_PIN 22
+#define US4_ECHO_PIN 23
+
+//Stepper Pins
+#define M1_PIN_1 29
+#define M1_PIN_2 30
+#define M1_PIN_3 31
+#define M1_PIN_4 32
+
+#define M2_PIN_1 33
+#define M2_PIN_2 34
+#define M2_PIN_3 35
+#define M2_PIN_4 36
+
+#define M3_PIN_1 37
+#define M3_PIN_2 38
+#define M3_PIN_3 39
+#define M3_PIN_4 40 
+
+#define M4_PIN_1 25
+#define M4_PIN_2 26 
+#define M4_PIN_3 27 
+#define M4_PIN_4 28
+
+//LED pins
+#define LED1_R 0
+#define LED1_G 1
+#define LED1_B 2
+
+#define LED2_R 3
+#define LED2_G 4
+#define LED2_B 5 
+
+#define LED2_R 6
+#define LED2_G 7 
+#define LED2_B 8
+
+#define LED2_R 9
+#define LED2_G 10
+#define LED2_B 11
+
+
+//#define PIR_PIN 7 //input for HC-s501
--- src/main.cpp
+++ src/main.cpp
@@ -3,39 +3,51 @@
 #include <Arduino.h>
 #include "SR04.h"
 #include "Stepper.h"
-#define TRIG_PIN 13
-#define ECHO_PIN 12 
+#include "pins.h"
+//#define TRIG_PIN 15
+//#define ECHO_PIN 14 
 
 //Define state machine variables that will control the state of the machine
 int state = 0;
 
 
-SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
-long a;
+SR04 sr04_1 = SR04(US1_ECHO_PIN, US1_TRIG_PIN);
+SR04 sr04_2 = SR04(US2_ECHO_PIN, US2_TRIG_PIN);
+SR04 sr04_3 = SR04(US3_ECHO_PIN, US3_TRIG_PIN);
+SR04 sr04_4 = SR04(US4_ECHO_PIN, US4_TRIG_PIN);
+long dist_1, dist_2, dist_3, dist_4;
 int steps;
-int pirPin = 7; // Input for HC-S501
-int pirValue; // Place to store read PIR Value
 
 const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
 const int rolePerMinute = 15;         // Adjustable range of 28BYJ-48 stepper is 0~17 rpm
 
 // initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
+Stepper myStepper(stepsPerRevolution, M1_PIN_1, M1_PIN_2, M1_PIN_3, M1_PIN_4);
 
 void setup() {
    myStepper.setSpeed(rolePerMinute);
-   pinMode(pirPin, INPUT);
    Serial.begin(9600);//Initialization of Serial Port
    delay(1000);
 }
 
 void loop() {
-   a=sr04.Distance();
-   Serial.print(a);
+   dist_1=sr04_1.Distance();
+   dist_2=sr04_2.Distance();
+   dist_3=sr04_3.Distance();
+   dist_4=sr04_4.Distance();
+   Serial.print
+   Serial.print(dist_1);
    Serial.println("cm");//The difference between "Serial.print" and "Serial.println" 
                         //is that "Serial.println" can change lines.
+   Serial.print(dist_1);
+   Serial.println("cm");
+   Serial.print(dist_1);
+   Serial.println("cm");
+   Serial.print(dist_1);
+   Serial.println("cm");
    while(a < 15)
    {
+     digitalReadFast
     a=sr04.Distance();
     if(steps < stepsPerRevolution)
     {
